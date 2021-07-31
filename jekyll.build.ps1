68e982d05ba1e9691410e0e41b20a1129b73e6e0
David Söderlund <ds@dsoderlund.consulting>
2021-08-01 00:47:36 +0200

    added build script
    Signed-off-by: David Söderlund <ds@dsoderlund.consulting>

---
+++ jekyll.build.ps1
@@ -0,0 +1,52 @@
+#Requires -Module invokebuild
+param(
+    $jekyllversion = '4',
+    $servecontainername = 'jekyll-serve'
+)
+$rootdir = git rev-parse --show-toplevel
+task new {
+    mkdir src -ea 0 | out-null
+    push-location $rootdir/src
+
+    #new
+    docker run --rm -it --volume="$($PWD):/srv/jekyll" --env JEKYLL_ENV=production jekyll/jekyll:4 jekyll new . --force
+
+    Pop-Location
+}
+
+task build {
+    if (test-path .\src) {
+        Push-Location $rootdir/src
+        #build
+        docker run --rm -it --volume="$($PWD):/srv/jekyll" --volume="$PWD/vendor/bundle:/usr/local/bundle" --env JEKYLL_ENV=production jekyll/jekyll:$jekyllversion jekyll build
+        Pop-Location
+    }
+    else {
+        throw "You cannot build what does not exist, run invokebuild new first!"
+    }
+}
+
+task remove stop, {
+    icm {docker rm $servecontainername } -ea 0
+}
+
+task stop {
+    icm {docker stop $servecontainername } -ea 0  
+}
+
+task serve stop, remove, {
+    if (test-path .\src) {
+        Push-Location $rootdir/src
+        #serve
+        docker run -d --name $servecontainername --volume="$($PWD):/srv/jekyll" --volume="$PWD/vendor/bundle:/usr/local/bundle" --env JEKYLL_ENV=development -p 4000:4000 jekyll/jekyll:$jekyllversion jekyll serve
+
+        Pop-Location
+    }
+    else {
+        throw "You cannot run what does not exist, run invokebuild new first!"
+    }
+}
+
+task run serve
+
+task . serve
