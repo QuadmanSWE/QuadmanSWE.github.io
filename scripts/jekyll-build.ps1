b0fbd7f335e404d440a27108e14cee333aeac87d
David <ds@dsoderlund.consulting>
2021-08-01 00:47:36 +0200

    added build script

--- scripts/jekyll-build.ps1
+++
@@ -1,6 +0,0 @@
-# This scripts runs the jekyll builder on docker
-# I use this on windows 10 with wsl2 and docker desktop.
-
-mkdir vendor/bundle -ea 0 | out-null
-
-docker run --rm -it --volume="$($PWD):/srv/jekyll" --volume="$PWD/vendor/bundle:/usr/local/bundle" --env JEKYLL_ENV=production jekyll/jekyll:4 jekyll build
