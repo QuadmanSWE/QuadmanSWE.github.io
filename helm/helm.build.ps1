861576544044f182c82c7ad48da03128f042772d
David <ds@dsoderlund.consulting>
2021-09-12 16:34:27 +0200

    improved build script

--- helm/helm.build.ps1
+++ helm/helm.build.ps1
@@ -1,5 +1,26 @@
+#Requires -Module invokebuild
+<#
+.SYNOPSIS
+    wrapper for helm using invoke-build tasks
+.DESCRIPTION
+    Can be used to create, lint, and package helm charts as well as publish them by copying and merging the repo in the dir exposed in github pages.
+.EXAMPLE
+    PS C:\> invoke-build new -chartname 'mychart'
+    Creates a new helm chart named mychart in the helm directory
+.EXAMPLE
+    PS C:\> invoke-build -chartname 'qr'
+    Lints, packages, and publishes the qr helm chart so that the tarball and index.yaml can be committed to the repo and thus upon merge to main be public.
+.EXAMPLE
+    PS C:\> invoke-build -url "http://localhost:4000/helm-charts"
+    Publishes the helm repo with the url as it shows up when serving jekyll for debug in docker
+.PARAMETER chartname
+    Name of the chart to act on, required for new, for other none means all
+.PARAMETER url
+    The url to use in the index.yaml, useful when debugging and not publishing to the blog.
+#>
 param(
-    $chartname
+    $chartname,
+    $url = "https://blog.dsoderlund.com/helm-charts"
 )
 $rootdir = git rev-parse --show-toplevel
 $chartsource = "$rootdir/helm/charts"
@@ -10,6 +31,15 @@ if ($null -eq $chartname) {
 }
 else {
     $chartnames = $chartname
+}
+
+task new {
+    if ($null -eq $chartname) {
+        throw ('you must supply a $chartname if you want to create this way')
+    }
+    else {
+        helm create "$chartsource/$chartname"
+    }
 }
 
 task lint {
@@ -40,9 +70,13 @@ task publish {
     }
     $tarballs | ForEach-Object {
         exec {
-            Move-Item -Path $_ -Destination $chartpublishdestination
+            Move-Item -Path $_ -Destination $chartpublishdestination -Force
         }
-    } 
+    }
+    exec {
+        helm repo index --merge index.yaml --url $url $chartpublishdestination
+        remove-item index.yaml #we don't need this extra temp file
+    }
 }
 task copy publish
 
